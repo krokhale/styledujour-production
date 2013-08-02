14f5f6e6de29608e783c516a2e97d16c39dc15e9
krishna rokhale <krishna.rokhale@gmail.com>
2013-08-02 10:02:18 +0530

    first commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Styledujour::Application.config.secret_key_base = 'c8d64085648046ae6d81681b012841e14f22927962daf08f34bdbe37dc64884be4c924ce8ea90f50342cbc11d21a25f4a14df2008de6d7ced80cc949671dc566'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Styledujour::Application.config.session_store :cookie_store, key: '_styledujour_session'
